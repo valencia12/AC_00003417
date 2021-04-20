a66cdeea751f3cdb1f0a6fc1ac196d7a09741361
Jorge Valencia <37429502+valencia12@users.noreply.github.com>
2021-04-19 23:31:45 -0600

    agregando sort

--- tarea.asm
+++ tarea.asm
@@ -8,24 +8,26 @@
         xor DX,DX
 
 
-        mov     byte [200h], "J"
-        mov     byte [201h], "G"
-        mov     byte [202h], "M"
-        mov     byte [203h], "V"
+        
+   
         
         ;Direccionamiento directo
         ;copiar el valor 200h a AX
         xor     AX,AX
+        mov     byte [200h], 6ah
         mov     AX, [200h]
         ;Copiar el valor de 201h a CX indrecto por registro
         xor     BX,BX
-        mov     BX, [201h]
+        mov     byte [201h], 67h
+        mov     BX, 201h
         mov     CX, [BX]
         ;Copiar el valor de 202h DX usando indirc base mas indice
         xor     BX,BX
-        mov     BX, [202h]
-        mov     DX, [BX + DI]
+        mov     byte [202h], 6dh
+        mov     BX, 202h
+        mov     DX, [BX + SI]
         ;Copiar el valor de 203h a DI usando dirc relativo por registro.
-        mov     DI, [SI + 203h]
+        mov     byte [203h], 76h
+        mov     DI, [SI + 3h]
         
         int 20h
