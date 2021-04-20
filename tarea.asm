e8191dae94cbccfb6573d772b05fb900a323c5a8
Jorge Valencia <37429502+valencia12@users.noreply.github.com>
2021-04-19 22:47:54 -0600

    Agregando xor

--- tarea.asm
+++ tarea.asm
@@ -2,6 +2,12 @@
 
         section .text
 
+        xor AX,AX
+        xor BX,BX
+        xor CX,CX
+        xor DX,DX
+
+
         mov     byte [200h], "J"
         mov     byte [201h], "G"
         mov     byte [202h], "M"
@@ -9,11 +15,14 @@
         
         ;Direccionamiento directo
         ;copiar el valor 200h a AX
+        xor     AX,AX
         mov     AX, [200h]
         ;Copiar el valor de 201h a CX indrecto por registro
+        xor     BX,BX
         mov     BX, [201h]
         mov     CX, [BX]
         ;Copiar el valor de 202h DX usando indirc base mas indice
+        xor     BX,BX
         mov     BX, [202h]
         mov     DX, [BX + DI]
         ;Copiar el valor de 203h a DI usando dirc relativo por registro.
